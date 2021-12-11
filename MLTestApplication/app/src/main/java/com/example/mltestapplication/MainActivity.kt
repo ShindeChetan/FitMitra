d48fcbbfe7fe830d6f5c26632bf57a01d0fddffd
Amlan Roy <amlanroy2500@gmail.com>
2021-12-11 15:12:17 +0530

    Comitting the initial files

---
+++ MLTestApplication/app/src/main/java/com/example/mltestapplication/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.mltestapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.annotation.SuppressLint
+import android.util.Log
+import android.util.Size
+import androidx.core.content.ContextCompat
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.video.Metadata
+import androidx.camera.view.video.OutputFileOptions
+import androidx.core.view.size
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.LifecycleOwner
+import com.example.mltestapplication.databinding.ActivityMainBinding
+import com.example.mltestapplication.utils.Draw
+import com.google.android.gms.tasks.Task
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.pose.Pose
+import com.google.mlkit.vision.pose.PoseDetection
+import com.google.mlkit.vision.pose.PoseDetector
+import com.google.mlkit.vision.pose.PoseLandmark
+import com.google.mlkit.vision.pose.defaults.PoseDetectorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var poseDetector :  PoseDetector
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        //getSupportActionBar()?.hide()
+
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            Log.d("CALLED", "Called inside cameraProviderFuture.addListener")
+            val cameraProvider = cameraProviderFuture.get()
+            bindPreview(cameraProvider = cameraProvider)
+        }, ContextCompat.getMainExecutor(this))
+
+//        val localModel = LocalModel.Builder()
+//            .setAbsoluteFilePath("pose_detection.tflite")
+//            .build()
+
+        val poseDetectorOptions =  PoseDetectorOptions.Builder()
+            .setDetectorMode(PoseDetectorOptions.STREAM_MODE)
+            .build()
+
+
+
+        poseDetector = PoseDetection.getClient(poseDetectorOptions)
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindPreview(cameraProvider: ProcessCameraProvider){
+        Log.d("CALLED", "Called Just inside Bind preview")
+        val preview =  Preview.Builder().build()
+
+
+        //defining the camera, the prerequisites before opening the camera
+        val cameraSelector = CameraSelector.Builder()
+            .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
+            .build()
+        preview.setSurfaceProvider(binding.previewView.surfaceProvider)
+
+
+
+        val imageAnalysis = ImageAnalysis.Builder()
+            .setTargetResolution(Size(1280,720))
+            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+            .build()
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this),{imageProxy ->
+            Log.d("CALLED", "Called inside imageAnalysis.setAnalyzer")
+            val rotationDegrees = imageProxy.imageInfo.rotationDegrees
+
+            val image = imageProxy.image
+
+            if(image != null){
+                val processImage = InputImage.fromMediaImage(image, rotationDegrees)
+                val result : Task<Pose> = poseDetector
+                        .process(processImage)
+                    .addOnSuccessListener { pose ->
+//                        val allPoseLandmarks = pose.allPoseLandmarks
+                        Log.d("CALLED", "Called inside poseDetector.onSuccessListener")
+                        if (binding.parentLayout.childCount > 1) binding.parentLayout.removeViews( 1,
+                            binding.parentLayout.childCount - 1)
+                            val element = Draw(context=this, pose)
+                            binding.parentLayout.addView(element)
+                        imageProxy.close()
+                    }.addOnFailureListener {
+                        Log.v("MainActivity", "Error - ${it.message}")
+                        imageProxy.close()
+                    }
+            }
+        })
+
+        cameraProvider.bindToLifecycle(this as LifecycleOwner, cameraSelector, imageAnalysis, preview)
+    }
+}
